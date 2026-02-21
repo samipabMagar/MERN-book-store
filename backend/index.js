e60c7d41a899ce50ecfaa95b9c4b28c8c8a1e2d3
samipabMagar <samipab98@gmail.com>
2026-02-21 22:43:53 +0545

    feat: add database connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,20 @@
-import express from 'express';
+import express from "express";
+import connection from "./models/index.js";
 
 const app = express();
 
-const PORT = 8000;
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+const PORT = process.env.PORT;
+
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+  try {
+    await connection.authenticate();
+    console.log("Database connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+});
