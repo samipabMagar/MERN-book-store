c771e77ef2122ee453dd62f6e42530498618bfc1
samipabMagar <samipab98@gmail.com>
2026-02-22 13:09:54 +0545

    feat: add multer for image upload and update book route to handle image files

--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -1,19 +1,50 @@
 import express from "express";
 import bookModel from "../models/bookModel.js";
+import multer from "multer";
 
 const router = express.Router();
 
-router.post("/add", async (req, res) => {
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    const imageName =
+      Date.now() +
+      "-" +
+      Math.round(Math.random() * 1e9) +
+      "-" +
+      file.originalname.trim();
+    cb(null, imageName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Only image files are allowed"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+router.post("/add", upload.single("image"), async (req, res) => {
   {
     try {
-      const data = await bookModel.create(req.body);
-      if(data) {
+      const data = await bookModel.create({
+        ...req.body,
+        image: req.file.filename,
+      });
+      if (data) {
         res.status(200).json({ message: "Book added successfully", data });
-      }
-      else {
+      } else {
         res.status(400).json({ message: "Failed to add book" });
       }
-      
     } catch (error) {
       console.error(error);
     }
